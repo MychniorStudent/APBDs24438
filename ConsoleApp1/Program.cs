934268fb59e2b8fab6fcd78df8e54062471eaf5a
s24438 <s24438@pjwstk.edu.pl>
2024-03-20 20:10:18 +0100

    dodanie metody liczącej srednia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,3 +3,10 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Commit1!");
 Console.WriteLine("Hello, Commit2!");
 Console.WriteLine("Hello, Commit3!");
+
+static float average(int[] tab)
+{
+    int sum = 0;
+    Array.ForEach(tab, x => sum += x);
+    return sum / tab.Length;
+}
